3e1ea9ea5001a31fba7ff99c90361d3bb7a8be0c
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-05 20:17:00 -0300

    .NET Core 3.0 + Azure Service Bus + Console Application
    .NET Core 3.0 + Azure Service Bus + Console Application

---
+++ ProcessadorMensagens/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.ServiceBus;
+
+namespace ProcessadorMensagens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json");
+            var configuration = builder.Build();
+
+            var client = new QueueClient(
+                configuration["AzureServiceBus:ConnectionString"],
+                configuration["AzureServiceBus:Queue"],
+                ReceiveMode.ReceiveAndDelete);
+            try
+            {
+                client.RegisterMessageHandler(
+                       async (message, token) =>
+                       {
+                           ProcessarCargaCotacoes(message);
+                       },
+                       new MessageHandlerOptions(
+                           async (e) =>
+                           {
+                               Console.WriteLine("[Erro] " +
+                                   e.Exception.GetType().FullName + " " +
+                                   e.Exception.Message);
+                           }
+                       )
+                );
+
+                Console.ReadKey();
+            }
+            finally
+            {
+                client.CloseAsync().Wait();
+            }
+
+            Console.WriteLine("Encerrando o processamento de mensagens!");
+        }
+
+        private static void ProcessarCargaCotacoes(
+            Message message)
+        {
+            var conteudo = Encoding.UTF8.GetString(message.Body);
+            Console.WriteLine(Environment.NewLine +
+                "[Nova mensagem recebida] " + conteudo);
+        }
+    }
+}
